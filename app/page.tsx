cb1caa8ee40dcf4955ad9b001732d04eb18f65a7
kiet.le <boyld222@gmail.com>
2024-06-23 14:17:29 +0700

    delete unneccesary file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Link from "next/link";
 import { draftMode } from "next/headers";
 
-import CoverImage from "./cover-image";
-import Avatar from "./avatar";
-import MoreStories from "./more-stories";
 
-import { getAllPosts, getTitleHero } from "@/lib/api";
-import { CMS_NAME, CMS_URL } from "@/lib/constants";
+import { getTitleHero } from "@/lib/api";
 import Loader from "./components/Layout/Loader";
 import HeroSection from "./components/Section/Hero/HeroSection";
 
