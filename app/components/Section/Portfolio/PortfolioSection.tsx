f3948179f1bfe3471fb0830fe064e0fee94304b7
kiet.le <kiet.le@ecom365.vn>
2024-06-25 11:40:37 +0700

    update carousel

--- app/components/Section/Portfolio/PortfolioSection.tsx
+++ app/components/Section/Portfolio/PortfolioSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -24,53 +24,64 @@ interface Props {
 
 export default function PortfolioSection({ section }: Props) {
   const newContentArray = Array(3).fill(section).flat(); // just triple the section for the carousel
+
+  //Need to use side effect to add class to parent element
+  useEffect(() => {
+    const specialChild = document.querySelector(".portfolio-wrapper");
+    if (specialChild && specialChild.parentElement) {
+      specialChild.parentElement.classList.add("portfolio-wrapper-parent");
+    }
+  }, []);
+
   return (
     <ContentLayout title="Portfolio" logoText="p" hasFooter>
-      <Swiper
-        watchSlidesProgress={true}
-        spaceBetween={48}
-        slidesPerView={3}
-        className="mySwiper"
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]}
-        // breakpoints={{
-        //   640: {
-        //     slidesPerView: 1,
-        //     spaceBetween: 0,
-        //   },
-        // }}
-      >
-        <div className="phomi-text">Phomi</div>
-        {newContentArray.reverse().map((slide, index) => (
-          <SwiperSlide key={index}>
-            {({ isActive, isNext }) => (
-              <div
-                className={clsx(
-                  `flex flex-col`,
-                  !isActive && !isNext ? "mt-[100px]" : "mt-0"
-                )}
-              >
-                <ContentfulImage
-                  alt={slide.image.description}
-                  className=" aspect-[4/3] w-[400px]"
-                  height={300}
-                  width={400}
-                  src={slide.image.url}
-                />
-                <span className="text-[32px] mb-2 text-center mt-10">
-                  {slide.title}
-                </span>
-                <p className="w-full whitespace-pre-line text-[16px] leading-[1.9] tracking-[0.02em] capitalize text-center">
-                  <span className="text-[#FF7E21]">●</span> {slide.role}
-                </p>
-              </div>
-            )}
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="w-full portfolio-wrapper">
+        <Swiper
+          watchSlidesProgress={true}
+          spaceBetween={48}
+          slidesPerView={1}
+          className="mySwiper"
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          breakpoints={{
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 0,
+            },
+          }}
+        >
+          <div className="phomi-text">Phomi</div>
+          {newContentArray.reverse().map((slide, index) => (
+            <SwiperSlide key={index}>
+              {({ isActive, isNext }) => (
+                <div
+                  className={clsx(
+                    `flex flex-col`,
+                    !isActive && !isNext ? "sm:mt-[100px]" : "mt-0"
+                  )}
+                >
+                  <ContentfulImage
+                    alt={slide.image.description}
+                    className=" aspect-[4/3] w-[400px]"
+                    height={300}
+                    width={400}
+                    src={slide.image.url}
+                  />
+                  <span className="text-[32px] mb-2 text-center mt-10">
+                    {slide.title}
+                  </span>
+                  <p className="w-full whitespace-pre-line text-[16px] leading-[1.9] tracking-[0.02em]  capitalize text-center">
+                    <span className="text-[#FF7E21]">●</span> {slide.role}
+                  </p>
+                </div>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </ContentLayout>
   );
 }
